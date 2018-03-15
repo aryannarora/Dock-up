e2cf74f815a8be8afabac45d5313868712dccb5f
Aryan arora <aryanarora278@gmail.com>
2018-03-16 04:35:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = (robot) => {
 
   robot.on('pull_request.closed', async context => {
 
-    if (context.payload.pull_request.merged_at != null)
+    if (context.payload.pull_request.merged_at != null) // We ignore the pull request that has not been merged
     {
       const token = context.github.auth.token
       const owner = context.payload.repository.owner.login
@@ -19,9 +19,9 @@ module.exports = (robot) => {
       if (messages) {
         getReadme(owner, repo, token)
         .then(result => {
-          const readme = Buffer((result.data.content), 'base64').toString('ascii')
+          const readme = Buffer((result.data.content), 'base64').toString('ascii') // decode the result
           //robot.log(readme.toUpperCase())
-          let content = []
+          let content = []  // an array for holding the non detected features or breaking changes.
 
            messages.forEach(function (item, index, array){
 
@@ -32,7 +32,7 @@ module.exports = (robot) => {
            if (content.length) {
             const issueSummary = {
               owner: owner,
-              body: content.join(', ') + '; not found in Documentation!',
+              body: content.join(', ') + ' ; not found in Documentation!',
               repo: repo,
               title: 'DOCUMENTATION NOT UP TO DATE',
               assignee: assignee,
