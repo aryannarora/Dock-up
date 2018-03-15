c4eadd67dc4d73a16a4f1624f1f0f9eaa391ea64
Aryan arora <aryanarora278@gmail.com>
2018-03-16 00:36:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const getReadme = require("./lib/get-readme.js")
 
 const createIssue = require("./lib/create-issue.js")
 
-module.exports = (robot) => {
-  robot.log('Yay, the app was loaded!')
 
-  robot.on('pull_request.closed', async context => {
+module.exports = (robot) => {
+
+  robot.on('pull_request', async context => {
+    
     const owner = context.payload.repository.owner.login
     const repo = context.payload.repository.name
     const number = context.payload.number
@@ -15,18 +16,21 @@ module.exports = (robot) => {
 
     const messages = await getCommitMessages(owner, repo, number) 
 
-    if (messages){
+    if (messages) {
       getReadme(owner, repo)
       .then(result => {
-      messages.forEach(function (item, index, array){
-        if (!result.data.includes(item)){
-          createIssue(owner, repo, "DOCUMENTATION NOT UP TO DATE", item + ' not found in Documentation!', assigne)
-          return
+        const readme = result.data    //Buffer((result.data.content), 'base64').toString('ascii')
+
+        let content = []
+
+         messages.forEach(function (item, index, array){
+
+         if (!readme.includes(item)){
+            content.push(item)
         }
       })
+         if (content.length) createIssue(owner, repo, "DOCUMENTATION NOT UP TO DATE", content.join(', ') + ' not found in Documentation!', assigne)
     })
     }
   })
 }
-
-
