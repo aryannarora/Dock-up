2c3452f02924668b88bb7dcca2c5a34bb2ecad26
Aryan Arora <aryanarora278@gmail.com>
2018-03-16 02:51:42 +0530

    auto closing of issues left

--- index.js
+++ index.js
@@ -2,31 +2,45 @@ const getCommitMessages = require("./lib/get-pr-commits.js")
 
 const getReadme = require("./lib/get-readme.js")
 
-const createIssue = require("./lib/create-issue.js")
-
 module.exports = (robot) => {
-  robot.log('Yay, the app was loaded!')
 
   robot.on('pull_request.closed', async context => {
-    const owner = context.payload.repository.owner.login
-    const repo = context.payload.repository.name
-    const number = context.payload.number
-    const assigne = context.payload.pull_request.user.login
 
-    const messages = await getCommitMessages(owner, repo, number) 
+    if (context.payload.pull_request.merged_at != null)
+    {
+      const token = context.github.auth.token
+      const owner = context.payload.repository.owner.login
+      const repo = context.payload.repository.name
+      const number = context.payload.number
+      const assignee = context.payload.pull_request.user.login
 
-    if (messages){
-      getReadme(owner, repo)
-      .then(result => {
-      messages.forEach(function (item, index, array){
-        if (!result.data.includes(item)){
-          createIssue(owner, repo, "DOCUMENTATION NOT UP TO DATE", item + ' not found in Documentation!', assigne)
-          return
-        }
+      const messages = await getCommitMessages(owner, repo, number, token) 
+      //console.log(messages)
+      if (messages) {
+        getReadme(owner, repo, token)
+        .then(result => {
+          const readme = Buffer((result.data.content), 'base64').toString('ascii')
+          //robot.log(readme.toUpperCase())
+          let content = []
+
+           messages.forEach(function (item, index, array){
+
+           if (!readme.toUpperCase().includes(item.toUpperCase())){
+            content.push(item)
+          }
+        })
+           if (content.length) {
+            const issueSummary = {
+              owner: owner,
+              body: content.join(', ') + '; not found in Documentation!',
+              repo: repo,
+              title: 'DOCUMENTATION NOT UP TO DATE',
+              assignee: assignee
+            }
+            context.github.issues.create(issueSummary)
+          }
       })
-    })
+      }
     }
   })
 }
-
-
